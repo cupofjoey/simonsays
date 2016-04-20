00a1a4bd70861208e85214230219de4e28aa2899
Joe Hill <joe@jkh3.com>
2016-04-20 19:42:47 -0400

    Logic guts for random sequence

--- simon.js
+++ simon.js
@@ -1,1 +1,15 @@
-alert("hello!");
+var sequence = [];
+
+
+var testButton = document.getElementById("test-button");
+var testFunction = function() {
+	var oneToFour = getRandomInt(1,4);
+	sequence.push(oneToFour);
+	console.log(sequence);
+};
+
+testButton.addEventListener("click", testFunction);
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random()* (max - min + 1)) + min
+};
