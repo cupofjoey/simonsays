4ed685661089cc51ab5a9f0ecec329fcfa15b12c
Joe Hill <joe@jkh3.com>
2016-04-20 21:06:22 -0400

    Compare Function is Complete, Probably One to Go to play

--- simon.js
+++ simon.js
@@ -1,6 +1,59 @@
 var sequence = [];
+addStep(); //game starts with 1 instruction
+console.log(sequence);
 
 var step = 0;
+
+var oneButton = document.getElementById("one-button");
+var twoButton = document.getElementById("two-button");
+var threeButton = document.getElementById("three-button");
+var fourButton = document.getElementById("four-button");
+
+var buttons = [oneButton, twoButton, threeButton, fourButton];
+
+var compareInput = function(number) {
+	var currentStep = sequence[step];
+	if(number == currentStep) {
+		console.log("Good Job!");
+		addStep();
+		step++;
+	}else{
+		console.log("Nope");
+		step = 0;
+	}
+	console.log(sequence);
+};	
+
+ for (var i = 1; i < 5; i++) {
+ 	var button = buttons[i - 1];
+ 	(function(num){
+ 		button.addEventListener("click", function(){	
+ 			compareInput(num);
+ 		});
+ 	})(i);	
+}
+
+var addStep = function() {
+	sequence.push(getRandomInt(1,4));
+};
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random()* (max - min + 1)) + min
+};
+
+
+var dummySteps = [1, 3, 4, 2, 3, 1];
+
+
+var startGame = function(){
+	var step = 0;
+	var currentStep = dummySteps[step];
+
+}
+
+
+//startGame();
+
 
 var testButton = document.getElementById("test-button");
 var testFunction = function() {
@@ -13,31 +66,3 @@ var testFunction = function() {
 testButton.addEventListener("click", function(){
 	testFunction();
 });
-
-var getInput = function(number) {
-	console.log(number);
-};	
-
-var oneButton = document.getElementById("one-button");
-var twoButton = document.getElementById("two-button");
-var threeButton = document.getElementById("three-button");
-var fourButton = document.getElementById("four-button");
-
-var buttons = [oneButton, twoButton, threeButton, fourButton];
-
- for (var i = 1; i < 5; i++) {
- 	var button = buttons[i - 1];
- 	(function(num){
- 		button.addEventListener("click", function(){	
- 			getInput(num);
- 		});
- 	})(i);	
-}
-
-var addStep = function() {
-	sequence.push(getRandomInt(1,4));
-};
-
-function getRandomInt(min, max) {
-	return Math.floor(Math.random()* (max - min + 1)) + min
-};
