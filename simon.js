36a9fc0b839cc460cd5da795284b2371c19438c2
Joe Hill <joe@jkh3.com>
2016-04-20 20:41:52 -0400

    Figured out the Sequencing in a For Loop

--- simon.js
+++ simon.js
@@ -1,14 +1,42 @@
 var sequence = [];
 
+var step = 0;
 
 var testButton = document.getElementById("test-button");
 var testFunction = function() {
-	var oneToFour = getRandomInt(1,4);
-	sequence.push(oneToFour);
+	addStep();
 	console.log(sequence);
+	console.log(sequence[step]);
+	step++;
 };
 
-testButton.addEventListener("click", testFunction);
+testButton.addEventListener("click", function(){
+	testFunction();
+});
+
+var getInput = function(number) {
+	console.log(number);
+};	
+
+var oneButton = document.getElementById("one-button");
+var twoButton = document.getElementById("two-button");
+var threeButton = document.getElementById("three-button");
+var fourButton = document.getElementById("four-button");
+
+var buttons = [oneButton, twoButton, threeButton, fourButton];
+
+ for (var i = 1; i < 5; i++) {
+ 	var button = buttons[i - 1];
+ 	(function(num){
+ 		button.addEventListener("click", function(){	
+ 			getInput(num);
+ 		});
+ 	})(i);	
+}
+
+var addStep = function() {
+	sequence.push(getRandomInt(1,4));
+};
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random()* (max - min + 1)) + min
